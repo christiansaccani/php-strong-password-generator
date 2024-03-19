e4ff8882cf2e98d2bf37432c11252e59a935da46
christiansaccani <christian.saccani99@gmail.com>
2024-03-19 15:27:00 +0100

    milestone 2 done

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+
+$password_length = isset($_GET['password_length']) ? $_GET['password_length'] : null;
+
+function generateRandomString($length) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!$%?';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[random_int(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
+
+?>
